743ca026b32c982c130f5d39e63362c48298a26e
varalaxmi <varadevops2121@gmail.com>
2024-11-22 18:40:54 +0530

    tf-roles

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -5,3 +5,4 @@ app_Version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/varadevops2121/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
+
